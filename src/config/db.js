d4dc0d14e16433de2ca2bdac41056adecbb0383e
Arfat Sayyed <sdarfat77@gmail.com>
2025-12-07 12:18:46 +0530

    DB Change To Amanox

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "MERN-Authentication",
+      dbName: 'amanox-database',
     });
-    console.log("✅ MongoDB Database connected successfully");
+    console.log('✅ MongoDB Database connected successfully');
   } catch (error) {
-    console.error("❌ MongoDB Database connection failed:", error);
+    console.error('❌ MongoDB Database connection failed:', error);
   }
 };
 export default connectDB;
